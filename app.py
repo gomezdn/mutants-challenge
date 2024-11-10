68974e257eb53bb4c7cfe6e66345d4784cfaa5bc
Dario <gomezdn@outlook.com>
2024-11-10 17:32:04 -0300

    feat: add api endpoints and db repository

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from utils.mutant import is_mutant
+from repository.mutant import get_stats, upsert_dna, get_dna
+
+
+api = Flask(__name__)
+
+@api.post("/mutant")
+def handle_mutant_post():
+    dna = request.json["dna"]
+    is_human = not is_mutant(dna)
+
+    upsert_dna(dna, is_human)
+
+    if (is_human):
+        return jsonify(False), 403
+    else:
+        return jsonify(True), 200
+
+@api.get("/stats")
+def handle_stats_get():
+    stats = get_stats()
+  
+    return jsonify(stats), 200
+
+@api.get("/dna/<hash>")
+def handle_get_dna(hash):
+    dna = get_dna(hash)
+
+    return dna, 200
---
+++ repository/mutant.py
@@ -0,0 +1,31 @@
+from xxhash import xxh64
+from db.db import dna_table
+
+def get_stats():
+  mutants = dna_table.select(count="exact").eq("is_mutant", "true").execute().count
+  humans = dna_table.select(count="exact").eq("is_mutant", "false").execute().count
+  ratio = max(mutants, 1) / max(humans, 1)
+
+  return {
+    "count_mutant_dna": mutants,
+    "count_human_dna": humans,
+    "ratio": round(ratio, 2)
+  }
+
+def upsert_dna(dna, mutant):
+  dna_string = "".join(dna)
+  hashed = xxh64(dna_string).hexdigest()
+
+  response = dna_table.upsert(
+    {
+    "dna_hash": hashed,
+    "dna": dna,
+    "is_mutant": mutant
+    }).execute()
+
+  return response
+
+def get_dna(dna_hash):
+  response = dna_table.select("dna", "is_mutant").eq("dna_hash", dna_hash).execute()
+
+  return response.data[0]
