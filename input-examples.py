543f2660016af7c53a556adc34f8aa0268d44476
Dario <gomezdn@outlook.com>
2024-11-10 17:43:01 -0300

    feat: add two input examples for the application

---
+++ input-examples.py
@@ -0,0 +1,17 @@
+MUTANT_DNA = [
+    "AEGCGG",
+    "ATGCLG",
+    "ATGGGG",
+    "ATCTGG",
+    "CCTCTE",
+    "TCACTG"
+]
+
+NON_MUTANT_DNA = [
+  "ATGCGA",
+  "CAGTGC",
+  "TTATTT",
+  "AGACGG",
+  "GCGTCA",
+  "TCACTG"
+]
